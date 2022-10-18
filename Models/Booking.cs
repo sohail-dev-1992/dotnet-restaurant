a0abb3448eaff0048407c19447b63a7ecfa68471
sohail <sohailbujhs@gmail.com>
2022-10-18 21:33:57 +0530

    restaurant changes added

---
+++ Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant.Models
+{
+    public class Booking
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string datepicker { get; set; }
+        public string email { get; set; }
+        public string number { get; set; }
+        public string time { get; set; }
+            
+    }
+}
